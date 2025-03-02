b590771988376bcbdc2060d55af4800232ca5f12
THARANI P S <127069926+Tharani-ux@users.noreply.github.com>
2025-03-02 09:38:35 +0530

    Delete harish directory

--- harish/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = "your_secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "static/uploads"
